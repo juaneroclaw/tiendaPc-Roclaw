a41299092d9cd3183849433fd0fb15e82fd67144
juaneroclaw <juaneroclaw@gmail.com>
2021-03-29 16:53:31 -0300

    Jroclaw-> Se modifico el nombre, se puso el nombre de lo que seria el Ecommers y el creador del mismo. Se cambio el color de fondo y se puso una imagen que identifica que el sitio esta en construcción

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './en-construccion.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hola soy Juan
+         Bienvenidos a Compu Market
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <samp>by Juan Roclaw</samp>
       </header>
     </div>
   );
